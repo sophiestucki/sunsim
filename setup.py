527bda4bdffba8841521de926b3d2eb11f40c7da
sophiestucki <stucki@ieec.cat>
2026-02-23 11:42:58 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="starsim",
+    version="0.1",
+    packages=find_packages(),  # automatically finds 'starsim' folder
+    install_requires=[
+        # add dependencies here if any, e.g., "numpy>=1.24"
+    ],
+)
